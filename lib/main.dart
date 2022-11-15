781ae06f125dcce34b65b020abcd427c3a79082b
Akanimoekong <akanimoekong@gmail.com>
2022-11-15 13:27:15 +0100

    Few Changes Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
+/*
+import 'oopclass.dart';
+
 void main() {
-  print( "Hello My World");
+  // print( "Hello My World");
   precious(6);
 // lambert(30,40);
 // akanimo();
 }
-
-
-
 void lambert (int theresa , int godswill){
+
   print(theresa+godswill);
 
    String buyCoke = "coke";
@@ -49,7 +50,9 @@ void akanimo(){
 }
 
 void precious (var pre){
-  var high = 20;
+
+*/
+/*  var high = 20;
   var low = 2;
 
   if (pre >= high){
@@ -59,6 +62,94 @@ void precious (var pre){
     print("We need Student ooo");
   }else{
     print("We are cool with more students");
+  }*//*
+
+
+*/
+/*  var animal = Animal();
+    animal.age = "12";
+    print(animal.age);*//*
+
+
+
+  var animal = Animal("Lion", "Yellow and Brown",
+      "Forest", " 1 Meter",
+      " 10 Years", "100 years");
+
+    print(
+        "${animal.animalName}\n"
+        "${animal.skinColor}\n"
+        "${animal.habitat}\n"
+        "${animal.height}\n"
+        "${animal.age} \n"
+        "${animal.lifeSpan}"    );
+}
+*/
+
+// Declaration of Class
+import 'dart:io';
+
+import 'package:for_learning/oopclass.dart';
+
+class Car {
+  late String akanimo;
+
+  // Declaration of Objects of the Class
+  Car(String engine) {
+    print(engine);
   }
 
 }
+
+void main() {
+  String? name = "";
+  String? color = "";
+  String? place = "";
+  String? length = "";
+  String? ages = "";
+  String? year = "";
+
+
+  Animal newAnim =  Animal(name, color, place, length, ages, year);
+
+  print("Enter Name");
+  name = stdin.readLineSync();
+
+  print("Enter Color");
+  color = stdin.readLineSync();
+
+  print("Enter Habitat");
+  place = stdin.readLineSync();
+
+  print("Enter Height");
+  length = stdin.readLineSync();
+
+  print("Enter Age");
+  ages = stdin.readLineSync();
+
+  print("Enter Lifespan");
+  year = stdin.readLineSync();
+
+  print("The Following are the featuress of $name ");
+
+  print("$color , $place , $length , $ages , $year");
+
+
+
+}
+
+
+
+// importing dart:io file
+/*
+import 'dart:io';
+
+void main()
+{
+
+  print("Enter your name?");
+  String? name = stdin.readLineSync();
+
+  print("Hello, $name! \nWelcome to AfricInnovate!!");
+}*/
+
---
+++ lib/oopclass.dart
@@ -0,0 +1,19 @@
+
+class Animal {
+
+   String animalName;
+   String skinColor;
+   String habitat;
+   String height;
+   String age;
+   String lifeSpan;
+
+  Animal(this.animalName,
+      this.skinColor,
+      this.habitat,
+      this.height,
+      this.age,
+      this.lifeSpan);
+
+
+}
